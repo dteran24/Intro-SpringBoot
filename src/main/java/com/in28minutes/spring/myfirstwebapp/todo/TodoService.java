e862452a8b6f31494d752812060e861aa5139bba
Daniel.Teran <Daniel.Teran@cognizant.com>
2022-09-24 13:25:19 -0500

    Intro to SpringBoot and JPA

---
+++ src/main/java/com/in28minutes/spring/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,48 @@
+package com.in28minutes.spring.myfirstwebapp.todo;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
+
+    static {
+        todos.add(new Todo(++todosCount, "daniel", "learn", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "daniel", "learn stuff", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(++todosCount, "daniel", "learn other stuff", LocalDate.now().plusYears(3), false));
+    }
+
+    public List<Todo> findByUsername(String username){
+        Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
+        return todos.stream().filter(predicate).toList();
+    }
+    public void addTodo(String username, String description, LocalDate targetdate, boolean isCompleted){
+        Todo todo = new Todo(++todosCount,username,description,targetdate,isCompleted);
+        todos.add(todo);
+
+    }
+
+    public void deleteTodo(int id){
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteTodo(todo.getId());
+        todos.add(todo);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'daniel', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'daniel', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'daniel', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'daniel', 'Learn DevOps', CURRENT_DATE(), false);
